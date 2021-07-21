2c3c6a1bc49b5fe7408ed56c7ae906ae6eb9ceef
Vlad <m_uladzislau2000@mail.ru>
2021-07-21 22:38:54 +0300

    django task PC shop

---
+++ neoviso/shop/shop/pcshopp/admin.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+
+from .models import*
+
+admin.site.register(Category)
+admin.site.register(Motherboard)
+admin.site.register(Cpu)
+admin.site.register(Ram)
+admin.site.register(Powersupply)
+admin.site.register(Videocard)
+admin.site.register(CartProduct)
+admin.site.register(Cart)
+admin.site.register(Customer)
+
+
+
